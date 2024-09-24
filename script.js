df375fb19215e8e0e5d485cb8a285190cea1e1e4
Gosselin Julien <j.gosselin678@gmail.com>
2024-09-24 16:13:08 +0200

    Modif+display

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 document.getElementById("myForm").addEventListener("submit", function(event) {
     event.preventDefault();
 
-    let nom = document.getElementById("#nom").value;
-    let prenom = document.getElementById("#prenom").value;
-    let genre = document.getElementById("#genre").value;
-    let dateNaissance = document.getElementById("#date").value;
-    let codePostal = document.getElementById("#codePostal").value;
-    let adresse = document.getElementById("#adresse").value;
-    let ville = document.getElementById("#ville").value;
-    let email = document.getElementById("#email").value;
-    let sujet = document.getElementById("#sujet").value;
-    let votreQuestion = document.getElementById("#votreQuestion").value;
+    let nom = document.getElementById("nom").value;
+    let prenom = document.getElementById("prenom").value;
+    let genre = document.getElementById("genre").value;
+    let dateNaissance = document.getElementById("date").value;
+    let codePostal = document.getElementById("codePostal").value;
+    let adresse = document.getElementById("adresse").value;
+    let ville = document.getElementById("ville").value;
+    let email = document.getElementById("email").value;
+    let sujet = document.getElementById("sujet").value;
+    let votreQuestion = document.getElementById("votreQuestion").value;
     
     localStorage.setItem("nom", nom);
     localStorage.setItem("prenom", prenom);
@@ -23,8 +23,6 @@ document.getElementById("myForm").addEventListener("submit", function(event) {
     localStorage.setItem("sujet", sujet);
     localStorage.setItem("votreQuestion", votreQuestion);
 
-    window.clientInformation.href = "display.html"
-
-    document.getElementById("displayNom").innerText = localStorage.getItem('nom');
-})
+    window.location.href = "display.html";
+});
 
